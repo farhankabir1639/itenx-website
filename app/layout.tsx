032488d308da4c8b001119ec1a9b5d7de6d68113
farhankabir1639 <kabirfarhan2@gmail.com>
2026-02-12 08:17:59 +0600

    feat: add Google Tag Manager with proper JSX syntax

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { Syne, DM_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -111,6 +112,22 @@ export default function RootLayout({
       <body
         className={`${syne.variable} ${dmSans.variable} bg-[#0A0C10] text-slate-100 font-sans min-h-screen antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVDQRLSD"
+            height={0}
+            width={0}
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NVDQRLSD');`,
+          }}
+        />
         <OrganizationJsonLd />
         <a
           href="#main-content"
